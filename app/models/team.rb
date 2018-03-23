c25393f732f74bf469e526343ead734f7b9476f3
Paolo D'Apice <dapicester@gmail.com>
2018-03-23 23:51:03 +0800

    Add team score

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,4 +2,15 @@ class Team < ApplicationRecord
   has_many :votes, inverse_of: :team, dependent: :destroy
 
   validates :name, presence: true
+
+  def score
+    avg = ->(col) { "AVG(#{col}) as #{col}" }
+    votes.select(
+      avg[:taste],
+      avg[:meal_planning],
+      avg[:presentation],
+      avg[:creativity],
+      avg[:kitchen_cleanliness]
+    ).first
+  end
 end
